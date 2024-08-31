062281c8a42a2f1941e612fd1b42e16f59690cb2
priyanka2334 <priyankadiwakar2024@gmail.com>
2024-08-31 12:25:27 +0530

    routes

--- router/src/App.js
+++ router/src/App.js
@@ -73,24 +73,73 @@
 
 
 
-import React from 'react';
-import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
-import About from './component/About';
-import Contect from './component/Contect';
-import Home from './component/Home';
+// import React from 'react';
+// import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
+// import About from './component/About';
+// import Contect from './component/Contect';
+// import Home from './component/Home';
 
+// function App(){
+//     return(
+//         <Router>
+//             <nav>
+//                 <Link to="/">About</Link>
+//                 <Link to="/Contect">Contect</Link>
+//                 <Link to="/Home">Home</Link>
+//             </nav>
+//             <Routes>
+//                 <Route path="/" element={<About/>} />
+//                 <Route path="/Contect" element={<Contect/>} />
+//                 <Route path="/Home" element={<Home/>} />
+//             </Routes>
+//         </Router>
+//     );
+// }
+// export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom";
+// import About from "./component/About";
+// import Home from "./component/Home";
+// import Contect from "./component/Contect";
+// function App(){
+//     return(
+//         <Router>
+//             <nav>
+//                 <link to="/">About</link>
+//                 <link to="/Contect">Contect</link>
+//                 <link to ="/Home">Home</link>
+//             </nav>
+//             <Routes>
+//                 <Route path="/" element={<About/>}/>
+//                 <Routes path="/Concect" element={<Contect/>}/>
+//                 <Routes path="/Home" element={<Home/>}/>
+//             </Routes>
+//         </Router>
+        
+//     );
+// }
+// export default App;
+
+
+import React from "react";
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
+import Home from "./component/Home";
+import Contect from "./component/Contect";
+import About from "./component/About";
 function App(){
     return(
         <Router>
             <nav>
-                <Link to="/">About</Link>
-                <Link to="/Contect">Contect</Link>
-                <Link to="/Home">Home</Link>
+                <Link to = "/">Home</Link>
+                <Link to ="/Contect">Concect</Link>
+                <Link to="/About">About</Link>
+                
             </nav>
             <Routes>
-                <Route path="/" element={<About/>} />
-                <Route path="/Contect" element={<Contect/>} />
-                <Route path="/Home" element={<Home/>} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/Contect" element={<Contect/>}/>
+                <Route path="/About" element={<About/>}/>
             </Routes>
         </Router>
     );
